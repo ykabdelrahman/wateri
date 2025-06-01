b85321f06db343f74d6302d4e5fb9568ce3e24db
Abdelrahman Hussein <ykabdelrahmanhussein@gmail.com>
2025-06-01 04:26:13 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:wateri/features/home/presentation/views/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeView());
+  }
+}
